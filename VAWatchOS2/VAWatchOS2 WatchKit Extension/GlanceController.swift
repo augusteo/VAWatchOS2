5246bd7d13d100bb024ab1fb1e0a1fb87cd2c02b
Victor Augusteo <augusteo@gmail.com>
2015-06-10 17:50:42 -0700

    added xcodeproj

---
+++ VAWatchOS2/VAWatchOS2 WatchKit Extension/GlanceController.swift
@@ -0,0 +1,31 @@
+//
+//  GlanceController.swift
+//  VAWatchOS2 WatchKit Extension
+//
+//  Created by victor augusteo on 10/06/2015.
+//  Copyright © 2015 augusteo. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class GlanceController: WKInterfaceController {
+
+    override func awakeWithContext(context: AnyObject?) {
+        super.awakeWithContext(context)
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ VAWatchOS2/VAWatchOS2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VAWatchOS2
+//
+//  Created by victor augusteo on 10/06/2015.
+//  Copyright © 2015 augusteo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
